d7540379e0b2719e0273f6290882c468291eabdc
Marilu Llamocca <maryleo.3007@gmail.com>
2017-03-01 10:46:57 -0500

    agregando segundo numero

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 // Recibiendo datos:
 var operacion= document.getElementById("operacion").value;
 var num1 = document.getElementById("num1").value;
+var num2 = document.getElementById("num2").value;
+
 switch(operacion){
   case "suma":
   return num1+num2;
